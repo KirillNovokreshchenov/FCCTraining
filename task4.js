4258141e040671af3cef156d7f813fedfc4a1ad4
KirillNovokreshchenov <KirillNovokrest@gmail.com>
2025-01-15 16:45:56 +0300

    Task4

---
+++ task4.js
@@ -0,0 +1,25 @@
+//
+// const character = "#";
+// const count = 8;
+// const rows = [];
+//
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(i);
+// }
+// console.log(rows)
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+// let result = ""
+// console.log(result)
+
+for(const i of rows){
+  console.log( typeof i)
+}
